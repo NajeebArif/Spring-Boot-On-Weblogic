06014d246e902acb81d2710cff416b9ca77cec83
Najeeb Arif <NajeebArif786@outlook.com>
2020-04-04 22:00:44 +0530

    Initial Project files
    Initial project files as created by spring initializer

---
+++ src/main/java/narif/poc/wlseries/echorestapi/EchoRestApiApplication.java
@@ -0,0 +1,13 @@
+package narif.poc.wlseries.echorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/narif/poc/wlseries/echorestapi/EchoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package narif.poc.wlseries.echorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
